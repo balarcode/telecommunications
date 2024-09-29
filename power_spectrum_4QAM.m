6e0c8f25c687d2246e8755338b1bf0b5bda0a853
balarcode <reachbalar@gmail.com>
2024-09-29 01:05:20 -0700

    Annotate Code for Power Spectral Density for 4-QAM Modulation
    Signed-off-by: balarcode <reachbalar@gmail.com>

--- power_spectrum_4QAM.m
+++ power_spectrum_4QAM.m
@@ -1,9 +1,9 @@
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Title     : Power Spectral Density for 4-QAM modulation that uses Gray encoding
-% Author	: balarcode
-% Version	: 1.0
-% Date		: 28th September 2024
+% Author	  : balarcode
+% Version	  : 1.1
+% Date		  : 29th September 2024
 % File Type : Matlab Script
 % File Test : Verified on Matlab R2024b
 % Comments  : From the power spectral density spectrum, it can be noticed that the 
@@ -30,10 +30,10 @@ M = 4;    % Constellation size
 %       And each of 10^5 symbols take one of the different amplitude levels 'M' as defined in the constellation diagram.
 k = log2(M); % Bits per symbol
 width = 20; % Width of a pulse shaping function
-Pulse_Shaping_Func = window(@rectwin, width); 
+pulse_shaping_function = window(@rectwin, width); 
 % NOTE: Generates a rectangular window function of width = 20.
 %       The Pulse Shaping Function obtained would be a column vector of size = 20x1.
-%       Pulse_Shaping_Func defined above is equivalent to defining an other function: ones(width,1).
+%       pulse_shaping_function defined above is equivalent to defining an other function: ones(width, 1).
 Tsym = 2; % Tsym is chosen to be 2 seconds.
 
 % Define the real and imaginary PAM constellation for 4-QAM
@@ -44,26 +44,26 @@ k_4QAM =  1 / sqrt(2 * (M-1) / 3);
 
 % Symbol Generation
 % -----------------------
-ipBit = rand(1,N*k,1)>0.5; % Random 1's and 0's
-ipBitReshape = reshape(ipBit,k,N).';
+bits = rand(1, N*k, 1) > 0.5; % Random 1's and 0's
+bitsReshape = reshape(bits, k, N).';
 bin2DecMatrix = ones(N,1)*(2.^((k/2-1):-1:0)) ; % Conversion from binary to decimal
-% real
-ipBitRe =  ipBitReshape(:,(1:k/2));
-ipDecRe = sum(ipBitRe.*bin2DecMatrix,2);
-ipGrayDecRe = bitxor(ipDecRe,floor(ipDecRe/2));
-% imaginary
-ipBitIm =  ipBitReshape(:,(k/2+1:k));
-ipDecIm = sum(ipBitIm.*bin2DecMatrix,2);
-ipGrayDecIm = bitxor(ipDecIm,floor(ipDecIm/2)); 
-% mapping the Gray coded symbols into constellation
-modRe = real_value(ipGrayDecRe+1);
-modIm = imag_value(ipGrayDecIm+1);
+% Process real part
+bitsReal = bitsReshape(:,(1:k/2));
+decimalReal = sum(bitsReal.*bin2DecMatrix,2);
+grayDecimalReal = bitxor(decimalReal, floor(decimalReal/2));
+% Process imaginary part
+bitsImag = bitsReshape(:,(k/2+1:k));
+decimalImag = sum(bitsImag.*bin2DecMatrix,2);
+grayDecimalImag = bitxor(decimalImag, floor(decimalImag/2)); 
+% Mapping the Gray coded symbols into constellation
+modRe = real_value(grayDecimalReal+1);
+modIm = imag_value(grayDecimalImag+1);
 
 % Complex Constellation
 mod = modRe + (1i * modIm);
 mod1 = k_4QAM * mod; % Normalization of transmit power to one 
-mod2 = mod1' * Pulse_Shaping_Func'; % Each symbol is multiplied by pulse shaping function which is a rectangular function.
-mod_reshaped = reshape(mod2',1,size(mod2,1)*size(mod2,2));
+mod2 = mod1' * pulse_shaping_function'; % Each symbol is multiplied by pulse shaping function which is a rectangular function.
+mod_reshaped = reshape(mod2', 1, size(mod2,1)*size(mod2,2));
 
 length_mod_reshaped = length(mod_reshaped);
 
@@ -74,19 +74,19 @@ psd = psd1(1:(length_mod_reshaped/2)).^2;
 
 freq = [0:length_mod_reshaped/2-1] / Tsym;
 
-% Figure(s) plotting
-% --------------------------
+% Figure(s)
+% ---------
 close all; 
 figure
 subplot(2,1,1);
-semilogy(freq, psd); % Plot on a semilog scale
+semilogy(freq, psd); % Plot on a semilog (base-10 logarithmic) scale
 title('Power Spectral Density plot on a logarithmic scale for 4-QAM');
 grid on;
 xlabel('Normalized Frequency');
 ylabel('Power Spectral Density');
 
 subplot(2,1,2);
-plot(freq,psd); 
+plot(freq, psd); 
 title('Power Spectral Density plot on a linear scale for 4-QAM');
 grid on;
 xlabel('Normalized Frequency');
